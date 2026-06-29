613ae1212e709940d98115078e114ee2f538e113
Kouadio <kkgephrem@gmail.com>
2026-06-29 06:42:52 +0000

    Restauration frontend comparateur de prix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import API_URL from "./config";
-
 function App() {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     fetch(`${API_URL}/products`)
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error("Erreur API:", err));
   }, []);
-
   return (
     <div>
       <h1>Liste des produits</h1>
@@ -24,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
